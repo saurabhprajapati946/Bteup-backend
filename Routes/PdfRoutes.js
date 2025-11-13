780aa95085e16b7ba27738275594114bf80ea9e9
saurabhprajapati946 <saurabhprajapati946@gmail.com>
2025-11-13 21:46:32 +0530

    Delete Routes directory

--- Routes/PdfRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/pdfController");
-
-router.post("/single", controller.singlePDF);
-router.post("/range", controller.rangePDF);
-
-module.exports = router;
