ca03ddffad68303f5ae7f7e2ad8e69895e8d5ccb
saurabhprajapati946 <saurabhprajapati946@gmail.com>
2025-11-13 20:04:37 +0530

    Create PuppeteerEngine.js

---
+++ Utils/PuppeteerEngine.js
@@ -0,0 +1,31 @@
+const puppeteer = require("puppeteer");
+const config = require("../config");
+
+exports.generatePDF = async (roll, type) => {
+  const url = config.baseURLs[type] + roll;
+
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ["--no-sandbox", "--disable-gpu"],
+  });
+
+  const page = await browser.newPage();
+  await page.goto(url, {
+    waitUntil: "networkidle2",
+    timeout: config.timeout
+  });
+
+  const check = await page.evaluate(() => document.body.innerText);
+  if (check.length < 100) {
+    await browser.close();
+    throw new Error("Invalid or empty page");
+  }
+
+  const pdf = await page.pdf({
+    format: "A4",
+    printBackground: true
+  });
+
+  await browser.close();
+  return pdf;
+};
